fd62552506bc40d185a2c63377cb66de498a5ee4
Nicolas <nicolasmeireles1004@gmail.com>
2024-04-19 19:25:48 -0300

    feat:

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,6 +2,8 @@ import './bootstrap';
 
 import Alpine from 'alpinejs';
 
+import '~bootstrap/js/bootstrap.bundle.min.js'
+
 window.Alpine = Alpine;
 
 Alpine.start();
--- resources/views/components/alert.blade.php
+++ resources/views/components/alert.blade.php
@@ -2,18 +2,21 @@
 @if(session()->has('info'))
     <div class="alert alert-info" role="alert">
         {{ session('info') }}
+        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
     </div>
 @endif
 
 @if(session()->has('success'))
     <div class="alert alert-success" role="alert">
         {{ session('success') }}
+        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
     </div>
 @endif
 
 @if(session()->has('error'))
     <div class="alert alert-danger" role="alert">
         {{ session('error') }}
+        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
     </div>
 @endif
 
@@ -31,3 +34,15 @@
         @endforeach
     </div>
 @endif
+
+<script>
+    document.addEventListener('DOMContentLoaded', function() {
+        const alert = document.getElementById('alert');
+        setTimeout(function() {
+            // Encontra o botão de fechar dentro do alerta
+            const closeButton = alert.querySelector('.btn-close');
+            // Simula um clique no botão de fechar
+            closeButton.click();
+        }, 5000);
+    });
+</script>
